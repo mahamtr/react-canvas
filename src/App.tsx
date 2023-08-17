96bf82fafb2dc3f647780ad9eb9fabc04c6fc3cd
Maai Ham <51383278+mahamtr@users.noreply.github.com>
2023-08-17 10:31:55 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./styles.css";
 const d3 = require("d3-delaunay");
 const height = window.innerHeight;
 const width = window.innerWidth;
-const n = 200;
+const n = 300;
 
 const particles = Array.from({ length: n }, () => [
   Math.random() * width,
@@ -16,31 +16,40 @@ const drawVoronoi = (context: any) => {
     const voronoi = delaunay.voronoi([0.5, 0.5, width - 0.5, height - 0.5]);
     context.clearRect(0, 0, width, height);
 
-    //context.beginPath();
-    //delaunay.render(context);
-    //context.strokeStyle = "#ccc";
-    //context.stroke();
+    const V = [...voronoi.neighbors(0)];
 
     context.beginPath();
-    voronoi.render(context);
-    voronoi.renderBounds(context);
-    context.strokeStyle = "#000";
-    context.stroke();
-
-    context.fillStyle = "#0f0";
-    context.beginPath();
-    voronoi.renderCell(0, context);
+    context.globalAlpha = 1;
+    context.filter = "brightness(100%)";
+    const grd = context.createLinearGradient(0, 0, 0, height);
+    grd.addColorStop(0.1, "#c22ed0");
+    grd.addColorStop(0.9, "#5ffae0");
+    context.fillStyle = grd;
+    context.fillRect(0, 0, width, height);
+    context.globalCompositeOperation = "source-over";
     context.fill();
 
-    //context.beginPath();
-    //delaunay.renderPoints(context);
-    //context.fill();
+    context.beginPath();
+    context.globalAlpha = 0.5;
+    voronoi.render(context);
+    voronoi.renderBounds(context);
+    context.strokeStyle = "#FFFFFF";
+    context.stroke();
+
+    context.beginPath();
+    voronoi.renderCell(0, context);
+    context.filter = "brightness(250%)";
+    context.fill();
+
+    context.beginPath();
+    context.filter = "brightness(120%)";
+    for (const j of V) voronoi.renderCell(j, context);
+    context.fill();
   }
 
   context.canvas.ontouchmove = context.canvas.onmousemove = (event: any) => {
     event.preventDefault();
     particles[0] = [event.layerX, event.layerY];
-    //colorSelectedParticle(delaunay.find(event.layerX, event.layerY));
     update();
   };
 
@@ -62,7 +71,6 @@ export default function App() {
       ref={canvasRef}
       width={window.innerWidth}
       height={window.innerHeight}
-      onClick={(e) => {}}
     />
   );
 }
